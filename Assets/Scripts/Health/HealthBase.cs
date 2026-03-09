1b53bfd30ef3c1fd469a9124ddc6ac3824dce102
joaomazzarolo <41448205+joaomazzarolo@users.noreply.github.com>
2026-03-10 00:19:33 +0100

    added enemy and damage animation

--- Assets/Scripts/Health/HealthBase.cs
+++ Assets/Scripts/Health/HealthBase.cs
@@ -10,9 +10,15 @@ public class HealthBase : MonoBehaviour
     public int _currentLife;
     private bool _isDead = false;
 
+    [SerializeField] private FlashColor _flashColor;
+
     private void Awake()
     {
         Init();
+        if (_flashColor == null)
+        {
+            _flashColor = GetComponent<FlashColor>();
+        }
     }
 
     private void Init()
@@ -30,6 +36,10 @@ public class HealthBase : MonoBehaviour
         if (_currentLife <= 0)
         {
             Kill();
+        }
+        if (_flashColor != null)
+        {
+            _flashColor.Flash();
         }
 
     }
---
+++ Assets/Scripts/Utility/FlashColor.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlashColor : MonoBehaviour
+{
+    public List<SpriteRenderer> spriteRenderers;
+    public Color color = Color.red;
+    public float duration = .3f;
+
+    private void OnValidate()
+    {
+        spriteRenderers = new List<SpriteRenderer>();
+        foreach (var child in transform.GetComponentsInChildren<SpriteRenderer>())
+        {
+            spriteRenderers.Add(child);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Flash();
+        }
+    }
+
+    public void Flash()
+    {
+        foreach(var s in spriteRenderers)
+        {
+            s.DOColor(color, duration).SetLoops(2, LoopType.Yoyo);
+        }
+    }
+}
