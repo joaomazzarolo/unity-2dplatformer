074bc4a6b0d7639b9f42e9c51296b3acc2792e59
joaomazzarolo <41448205+joaomazzarolo@users.noreply.github.com>
2026-03-12 00:53:27 +0100

    added shooting animation from player and enemy damage

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -7,6 +7,7 @@ public class EnemyBase : MonoBehaviour
     public int damage = 2;
     public Animator animator;
     public string triggerAttack = "Attack";
+    public HealthBase healthbase;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -24,5 +25,10 @@ public class EnemyBase : MonoBehaviour
     private void PlayAttackAnimation()
     {
         animator.SetTrigger(triggerAttack);
+    }
+
+    public void Damage(int amount)
+    {
+        healthbase.Damage(amount);
     }
 }
